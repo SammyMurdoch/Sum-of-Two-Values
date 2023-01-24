57a356bf15fa6c75b1b6bd493f40fd2a76c7b7b1
Sammy Murdoch <sammy.murdoch.0@gmail.com>
2023-01-24 12:26:04 +0000

    Is now a class

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-def efficient_check_sum_in_list(target: int, int_list: list) -> list:  # Assume the list has no duplicate values
-    index_dict = {}
-    pairs = []
+class CheckSumInList:
+    def __init__(self, int_list: list, target: int):
+        self.int_list = int_list
+        self.target = target
 
-    for i, n in enumerate(int_list):
-        if n in index_dict:
-            pairs.append((i, index_dict[n]))
+    def no_duplicates(self) -> list:  # Assume the list has no duplicate values
+        index_dict = {}
+        pairs = []
 
-        required_pair = target - n
+        for i, n in enumerate(self.int_list):
+            if n in index_dict:
+                pairs.append((i, index_dict[n]))
 
-        if required_pair in index_dict:
+            required_pair = self.target - n
             index_dict[required_pair] = i
 
-        else:
-            index_dict[required_pair] = i
-
-    return pairs
+        return pairs
 
 
-print(efficient_check_sum_in_list(10, [1, 9, 5, 3, -55, -55, -55, 3, 65, 4]))
+print(CheckSumInList([1, 9, 8, 6, 2, -55, 10, 65], 10).no_duplicates())
